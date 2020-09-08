b88ea6c9ba3f5d7d0bc74e91efe921e68ccd6971
PanickedRooster <68332364+PanickedRooster@users.noreply.github.com>
2020-09-08 14:05:48 +1000

    Finished

--- solver.py
+++ solver.py
@@ -36,16 +36,20 @@ class LaserTankState:
         return self.id == other.id
 
     def __hash__(self):
+        # Open source list comprehension: https://piazza.com/class/kcbajvkyu8b7j4?cid=80
         stuff = [item for sublist in self.lasertank.grid_data for item in sublist]
+
+        # Open source list comprehension: https://piazza.com/class/kcbajvkyu8b7j4?cid=123
         return hash((self.lasertank.player_x, self.lasertank.player_y, self.lasertank.player_heading) + tuple(stuff))
 
     def __lt__(self, other):
         return self.value_for_priority < other.value_for_priority
 
     def get_neighborlist(self):
+        # Logic retrieved from tutor code: https://gist.github.com/tttor/826be15b99bb4b33a50787d7eb7b5fda
         neighborlist = []
         for action in self.moves:
-            data = [row[:] for row in self.lasertank.grid_data]
+            data = [x[:] for x in self.lasertank.grid_data]
             temp = LaserTankMap(self.lasertank.x_size, self.lasertank.y_size, data, self.lasertank.player_x,
                                 self.lasertank.player_y, self.lasertank.player_heading)
             temp.apply_move(action)
@@ -53,15 +57,10 @@ class LaserTankState:
             neighborlist.append((neighbor, action))
         return neighborlist
 
-    def estimate_cost_to_go(self, heuristic_mode):
-        if heuristic_mode == 'zeroed':
-            cost_to_go_estimate = 0
-        elif heuristic_mode == 'manhattan':
-            cost_to_go_estimate = abs(self.flag_pos[0] - self.lasertank.player_x)
-            cost_to_go_estimate += abs(self.flag_pos[1] - self.lasertank.player_y)
-        else:
-            raise NotImplementedError(heuristic_mode)
-        return cost_to_go_estimate
+    def future_cost(self):
+        euclidean = abs(self.flag_pos[0] - self.lasertank.player_x)
+        euclidean += abs(self.flag_pos[1] - self.lasertank.player_y)
+        return euclidean
 
 def write_output_file(filename, actions):
     """
@@ -77,90 +76,95 @@ def write_output_file(filename, actions):
     f.write('\n')
     f.close()
 
-def search_bfs(start):
-    begin_clock = time.time()
+
+def ucs(start):
+    """
+    Function Retrieved and modified from Open sourced: https://gist.github.com/tttor/826be15b99bb4b33a50787d7eb7b5fda
+    :param start: Start State
+    :return: Actions to Take to Solution
+    """
     log = dict()
-    log['nvextex_explored_(with_duplicates)'] = 0
-
-    explored = set()
-    fringe = Queue()
-    fringe.put((start, []))
-
-    while fringe.qsize() > 0:
-        current, path = fringe.get()
-        explored.add(current.id)
-
-        log['nvextex_explored_(with_duplicates)'] += 1
-        for neighbor, action in current.get_neighborlist():
-            if neighbor.lasertank.is_finished():
-                log['nvertex_in_fringe_at_termination'] = fringe.qsize()
-                log['nvextex_explored'] = len(explored)
-                log['action_path'] = path + [action]
-                log['elapsed_time_in_minutes'] = (time.time() - begin_clock)
-                print(log['elapsed_time_in_minutes'])
-                print(log['nvextex_explored'])
-                return log['action_path']
-            if neighbor.id in explored:
-                # print("skipped")
-                continue
-
-            fringe.put((neighbor, path + [action]))
-
-    # raise RuntimeError('No Solution!')
-
-def search_astar(start, heuristic_mode='manhattan'):
     begin_clock = time.time()
-    log = dict()
-    log['nvextex_explored_(with_duplicates)'] = 0
 
     fringe = PriorityQueue()
     fringe.put(start)
-    explored = {start.id: start.cost} # a dict of `vertex: cost_so_far`
-    path = {start.id: []} # a dict of `vertex: actions`
-    log['nvextex_explored_(with_duplicates)'] += 1
+    explored = {start.id: start.cost}  # a dict of `vertex: cost_so_far`
+    path = {start.id: []}  # a dict of `vertex: actions`
+    running_count = 0
 
     while fringe.qsize() > 0:
         current = fringe.get()
         if current.lasertank.is_finished():
             log['nvertex_in_fringe_at_termination'] = fringe.qsize()
             log['nvextex_explored'] = len(explored)
-            log['action_path'] = path[current.id]
-            log['solution_path_cost'] = explored[current.id]
             log['elapsed_time_in_minutes'] = (time.time() - begin_clock)
-            print(log['action_path'], log['elapsed_time_in_minutes'])
+            log['action_path'] = path[current.id]
+            for item in log.values():
+                print(item)
+            for item in log.keys():
+                print(item)
+            print("Running: ", running_count)
             return log['action_path']
 
         for neighbor, action in current.get_neighborlist():
+            running_count += 1
+            cost_so_far = explored[current.id] + current.cost
+            if (neighbor.id not in explored):
+                explored[neighbor.id] = cost_so_far
+                path[neighbor.id] = path[current.id] + [action]
+
+                vfp = cost_so_far
+                neighbor.value_for_priority = vfp
+                fringe.put(neighbor)
+
+
+def search_astar(start):
+    """
+    Function Retrieved from Open sourced: https://gist.github.com/tttor/826be15b99bb4b33a50787d7eb7b5fda
+    :param start: Start State
+    :return: Actions to Take to Solution
+    """
+    log = dict()
+    begin_clock = time.time()
+
+    fringe = PriorityQueue()
+    fringe.put(start)
+    explored = {start.id: start.cost} # a dict of `vertex: cost_so_far`
+    path = {start.id: []} # a dict of `vertex: actions`
+    running_count = 0
+
+    while fringe.qsize() > 0:
+        current = fringe.get()
+        if current.lasertank.is_finished():
+            log['nvertex_in_fringe_at_termination'] = fringe.qsize()
+            log['nvextex_explored'] = len(explored)
+            log['elapsed_time_in_minutes'] = (time.time() - begin_clock)
+            log['action_path'] = path[current.id]
+            for item in log.values():
+                print(item)
+            for item in log.keys():
+                print(item)
+            print("Running: ", running_count)
+            return log['action_path']
+
+        for neighbor, action in current.get_neighborlist():
+            running_count += 1
             cost_so_far = explored[current.id] + current.cost
             if (neighbor.id not in explored) or (cost_so_far < explored[neighbor.id]):
                 explored[neighbor.id] = cost_so_far
                 path[neighbor.id] = path[current.id] + [action]
-                log['nvextex_explored_(with_duplicates)'] += 1
 
-                vfp = cost_so_far + current.estimate_cost_to_go(heuristic_mode)
+                vfp = cost_so_far + current.future_cost()
                 neighbor.value_for_priority = vfp
                 fringe.put(neighbor)
+
 
 def main(arglist):
     input_file = arglist[0]
     output_file = arglist[1]
 
-    # Read the input testcase file
+    # Read the input 'testcase' file
     game_map = LaserTankMap.process_input_file(input_file)
-    flag_pos = (0,0)
-    for i in range(game_map.x_size):
-        for j in range(game_map.y_size):
-            if game_map.grid_data[j][i] == game_map.FLAG_SYMBOL:
-                flag_pos = (i, j)
-    enter = LaserTankState(game_map, 0, flag_pos)
-
-    actions = []
-
-    # print(game_map)
-
-    # actions.append(search_bfs(enter))
-    actions = search_astar(enter)
-    # actions.append(search_astar(enter))
 
     #
     #
@@ -171,10 +175,27 @@ def main(arglist):
     #
     #
 
+    # Find the flag position to be used for the manhattan heuristic function
+    flag_pos = (0,0)
+    for i in range(game_map.x_size):
+        for j in range(game_map.y_size):
+            if game_map.grid_data[j][i] == game_map.FLAG_SYMBOL:
+                flag_pos = (i, j)
+
+    # Wrap the initial instance and flag
+    enter = LaserTankState(game_map, 0, flag_pos)
+    actions = search_astar(enter)
+    # actions = search_bfs(enter)
+    actions = ucs(enter)
+
     # Write the solution to the output file
     write_output_file(output_file, actions)
 
 
 if __name__ == '__main__':
     main(sys.argv[1:])
+
+
+
+
 
